e3639954d37261837bf10766a9e07dfab52055f5
Amy Kapernick <amy@aimhigherweb.design>
2020-01-24 11:00:24 +0800

    made pa11y save to file

--- tests/pa11y.js
+++ tests/pa11y.js
@@ -1,13 +1,28 @@
-const pa11y = require('pa11y')
+const pa11y = require('pa11y'),
+puppeteer = require('puppeteer-core'),
+fs = require('file-system')
+
+//Define the browser settings at the start of your runTest function
+const browser = await puppeteer.launch({
+	ignoreHTTPSErrors: true,
+	executablePath: '/mnt/c/Program Files (x86)/Google/Chrome/Application/chrome.exe',
+	args: ['--no-sandbox'],
+})
 
 const runTest = async () => {
 	const results = await Promise.all([
 		// Basic pa11y test with no options
-		pa11y(`http://localhost:65519`),
+		pa11y(`http://localhost:65519`, {
+			browser: browser
+		}),
 	])
 
-	// Display Results
-	console.log(results)
+	// Save Results
+	fs.writeFile('tests/results/pa11y.json', JSON.stringify(results), err => {
+		console.log(err)
+	})
+
+	browser.close()
 }
 
 runTest()
