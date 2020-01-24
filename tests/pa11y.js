37f0da061fd0c0e08b92058675a82cd5fc352c09
Amy Kapernick <amy@aimhigherweb.design>
2020-01-24 11:05:07 +0800

    fixed header background

--- tests/pa11y.js
+++ tests/pa11y.js
@@ -2,14 +2,14 @@ const pa11y = require('pa11y'),
 puppeteer = require('puppeteer-core'),
 fs = require('file-system')
 
-//Define the browser settings at the start of your runTest function
-const browser = await puppeteer.launch({
-	ignoreHTTPSErrors: true,
-	executablePath: '/mnt/c/Program Files (x86)/Google/Chrome/Application/chrome.exe',
-	args: ['--no-sandbox'],
-})
-
 const runTest = async () => {
+	//Define the browser settings at the start of your runTest function
+	const browser = await puppeteer.launch({
+		ignoreHTTPSErrors: true,
+		executablePath: '/mnt/c/Program Files (x86)/Google/Chrome/Application/chrome.exe',
+		args: ['--no-sandbox'],
+	})
+
 	const results = await Promise.all([
 		// Basic pa11y test with no options
 		pa11y(`http://localhost:65519`, {
