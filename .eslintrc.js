7e8f39214a4f3f50fd1dab5a3c04a7bdd43f35c2
Amy Kapernick <amy@aimhigherweb.design>
2020-01-30 06:54:33 +0800

    eslint demo

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+	extends: [ "airbnb-base" ],
+	plugins: [  ],
+	rules: { 
+			'linebreak-style': 0,
+			'no-tabs': 0,
+			indent: ['error', 'tab', {
+				"SwitchCase": 1,
+				"VariableDeclarator": 1
+			}],
+			'arrow-spacing': ['error', { before: true, after: true }],
+			'comma-dangle': [
+				'error',
+				{
+					objects: 'only-multiline',
+					arrays: 'only-multiline',
+					imports: 'never',
+					exports: 'never',
+					functions: 'never',
+				},
+			],
+			'no-var': 'error',
+			'no-unused-vars': 1,
+			'one-var': [2, 'consecutive'],
+			quotes: ['error', 'backtick'],
+			'no-param-reassign': ["error", { "props": false }]
+	 }
+}
