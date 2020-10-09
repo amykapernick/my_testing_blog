8cf572a1361ae92cbddbd5ab497ed93adfd1436c
Amy Kapernick <amy@kapers.dev>
2020-10-09 11:17:45 +0800

    changed to markdown blogs and updates some styles

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,10 @@
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight"),
+{ DateTime } = require('luxon'),
+markdownIt = require('markdown-it')
+
+const slugs = (string) => (string.toLowerCase().replace(/(#|:|!|\?)/g, '').replace(/\s/g, '-').replace(/--/g, '-'))
+
+
 module.exports = eleventyConfig => {
 	// Static resources
 	eleventyConfig.addPassthroughCopy({ 'resources/img': 'img' })
@@ -8,17 +15,31 @@ module.exports = eleventyConfig => {
 
 	// Watch files
 	eleventyConfig.addWatchTarget('resources/scss/')
+	eleventyConfig.addWatchTarget('index.njk')
+
+	eleventyConfig.addFilter('shortDate', dateObj => {
+		return DateTime.fromJSDate(dateObj).toFormat('dd LLL')
+	})
+
+	eleventyConfig.addShortcode('slug', (title) => {
+		return slugs(title)
+	})
+
+	eleventyConfig.addPlugin(syntaxHighlight)
+
+	eleventyConfig.setTemplateFormats(['html', 'md', 'js', 'njk', 'png', 'jpg', 'css'])
+
+
+	eleventyConfig.setLibrary('md', 
+		markdownIt({
+			html: true,
+			breaks: true,
+			linkify: true
+		})
+	)
+
 	eleventyConfig.setBrowserSyncConfig({
 		notify: true,
 		watch: true,
 	})
-
-	return {
-		dir: {
-			data: '_data',
-		},
-		templateFormats: ['njk', 'html', 'css'],
-		htmlTemplateEngine: 'njk',
-		dataTemplateEngine: 'njk',
-	}
 }
--- filters/dates.js
+++
@@ -1,8 +0,0 @@
-/*
-  A date formatter filter for Nunjucks
-*/
-module.exports = function (date) {
-	const month = [`January`, `February`, `March`, `April`, `May`, `June`, `July`, `August`, `September`, `October`, `November`, `December`],
-	 d = new Date(date);
-	return `${d.getDate()} ${month[d.getMonth()].substr(0, 3)} ${d.getUTCFullYear()}`;
-};
